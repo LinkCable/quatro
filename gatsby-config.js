101d8d8252fc7550262a4dfd69f9636728c3e109
Philippe Kimura-Thollander <philippekimurathollander@gmail.com>
2021-06-15 22:26:46 -0700

    change home page

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,11 +1,11 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Starter Blog`,
+    title: `philkt`,
     author: {
-      name: `Kyle Mathews`,
-      summary: `who lives and works in San Francisco building useful things.`,
+      name: `Phil Kimura-Thollander`,
+      summary: `designer for oculus`,
     },
-    description: `A starter blog demonstrating what Gatsby can do.`,
+    description: `A personal portfolio.`,
     siteUrl: `https://gatsbystarterblogsource.gatsbyjs.io/`,
     social: {
       twitter: `philkt_`,
--- src/components/layout.js
+++ src/components/layout.js
@@ -8,9 +8,14 @@ const Layout = ({ location, title, children }) => {
 
   if (isRootPath) {
     header = (
-      <h1 className="main-heading">
-        <Link to="/">{title}</Link>
-      </h1>
+      <div>
+        <h1 className="main-heading">
+          <Link to="/">{title}</Link>
+        </h1>
+        <h2 className="main-heading">
+          <Link to="/blog">Blog</Link>
+        </h2>
+      </div>
     )
   } else {
     header = (
@@ -25,9 +30,7 @@ const Layout = ({ location, title, children }) => {
       <header className="global-header">{header}</header>
       <main>{children}</main>
       <footer>
-        © {new Date().getFullYear()}, Built with
-        {` `}
-        <a href="https://www.gatsbyjs.com">Gatsby</a>
+        Built different
       </footer>
     </div>
   )
