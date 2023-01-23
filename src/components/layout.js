472d6c240a058ef3d5c3f332176fde5215b346db
Phil KT <philippekimurathollander@gmail.com>
2023-01-23 12:21:49 -0800

    scrolling on iOS works!

--- src/components/layout.js
+++ src/components/layout.js
@@ -37,7 +37,7 @@ const Layout = ({ location, title, children, className }) => {
 
   
 
-  let [footerText, setFooter] = React.useState("the end");
+  let [footerText, setFooter] = React.useState("your message is loading");
   
   switch (Math.floor(Math.random() * (4))) {
     case 0:
